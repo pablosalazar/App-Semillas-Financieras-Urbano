8c89c6ab44c0fb5c95509bc2a05cbf32dd4e6857
Juan Pablo Salazar <pablo.salazar.restrepo@gmail.com>
2025-11-07 18:02:51 -0500

    feat(query): integrate React Query for data fetching and add ESLint plugin for query management

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { RouterProvider } from "react-router";
 
 import router from "@/router/routes";
-import { env } from "./config/env";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
-  console.log(env);
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
