51605ac7658c1902625ddae4bce92e11d4a90776
Juan Pablo Salazar <pablo.salazar.restrepo@gmail.com>
2025-11-30 11:33:47 -0500

    feat: Dynamically configure ATM transaction options with conditional navigation links and updated title based on operation type from context.

--- src/features/modules/9-cajero-automatico/pages/ChooseTransaction.tsx
+++ src/features/modules/9-cajero-automatico/pages/ChooseTransaction.tsx
@@ -1,54 +1,63 @@
 import { ATM } from "../components/atm/ATM";
-
-const config = {
-  title: "No retires tu tarjeta hasta que no finalice la transacción",
-  left: [
-    {
-      number: 1,
-      label: "Retiro cuenta corriente",
-      href: null,
-    },
-    {
-      number: 2,
-      label: "Retiro cuenta de ahorro",
-      href: null,
-    },
-    {
-      number: 3,
-      label: "Transferencia entre cuentas",
-      href: null,
-    },
-    {
-      number: 4,
-      label: "Cambio de clave",
-      href: null,
-    },
-  ],
-  right: [
-    {
-      number: 1,
-      label: "Saldo cuenta corriente",
-      href: null,
-    },
-    {
-      number: 2,
-      label: "Saldo cuenta de ahorro",
-      href: null,
-    },
-    {
-      number: 3,
-      label: "Retiro ahorro a la mano",
-      href: "/modulos/cajero-automatico/ingresa-tu-clave",
-    },
-    {
-      number: 4,
-      label: "Otras transacciones",
-      href: null,
-    },
-  ],
-  screen: <div>hehe</div>,
-};
+import { ATM_BASE_PATH, ATM_OPERATIONS, ATM_ROUTES } from "../constants/atm";
+import { useATM } from "../context/ATMContext";
 
 export default function ChooseTransaction() {
+  const { operationType } = useATM();
+
+  const config = {
+    title: "Selecciona la transacción",
+    left: [
+      {
+        number: 1,
+        label: "Retiro cuenta corriente",
+        href: null,
+      },
+      {
+        number: 2,
+        label: "Retiro cuenta de ahorro",
+        href:
+          operationType === ATM_OPERATIONS.WITHDRAW_MONEY
+            ? `${ATM_BASE_PATH}/${ATM_ROUTES.ENTER_PIN}`
+            : null,
+      },
+      {
+        number: 3,
+        label: "Transferencia entre cuentas",
+        href: null,
+      },
+      {
+        number: 4,
+        label: "Cambio de clave",
+        href: null,
+      },
+    ],
+    right: [
+      {
+        number: 1,
+        label: "Saldo cuenta corriente",
+        href: null,
+      },
+      {
+        number: 2,
+        label: "Saldo cuenta de ahorro",
+        href:
+          operationType === ATM_OPERATIONS.CHECK_BALANCE
+            ? `${ATM_BASE_PATH}/${ATM_ROUTES.CHECK_BALANCE}`
+            : null,
+      },
+      {
+        number: 3,
+        label: "Retiro ahorro a la mano",
+        href: null,
+      },
+      {
+        number: 4,
+        label: "Otras transacciones",
+        href: null,
+      },
+    ],
+  };
+
   return <ATM config={config} />;
 }
